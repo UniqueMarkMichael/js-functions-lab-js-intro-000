ade7e4c35ad269f89f41a4abe1370cc3598ce70b
Unique Mark Michael <uniquemarkmichael@gmail.com>
2020-03-23 16:31:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function happyCustomHolidayTo(holiday, name){
 return (`Happy ${holiday}, ${name}!`)
 
 }
+
+function holidayCountdown(holiday, days){
+return ("It\'s ${days} days until ${holiday}!")
+
+}
+
+
+
