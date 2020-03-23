6154153b1ce146c4983b9e51be76e49c6f7b0d2a
Unique Mark Michael <uniquemarkmichael@gmail.com>
2020-03-23 15:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(Ketan){
   return (`Happy holidays, ${Ketan}!`)
 }
 
-function happyCustomHolidayTo(string){
+function happyCustomHolidayTo(Halloween){
   return (`Happy ${Halloween}, ${Ketan}!`)
 
 }
